ba8fcaac65ae5649025bbf8239e27939542243bf
garisian <garisian22@gmail.com>
2017-09-01 13:54:39 -0400

    reorganized file structure

--- content.js
+++
@@ -1,6 +0,0 @@
-// Get the details of the page and send it back 
-chrome.runtime.sendMessage({
-    'title': document.title,
-    'url': window.location.href,
-    'summary': window.getSelection().toString()
-});
--- event.js
+++
@@ -1,9 +0,0 @@
-// Inject the content script to extract data and then do a callback
-function getPageDetails(callback) 
-{ 
-    chrome.tabs.executeScript(null, { file: 'content.js' }); 
-    chrome.runtime.onMessage.addListener(function(message) 
-    { 
-        callback(message); 
-    }); 
-};
